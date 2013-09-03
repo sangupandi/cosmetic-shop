d8cb84cd5d0dc890c846407cf15c372ec5e193dc
Mert Yerli <mertyerli@gmail.com>
2013-09-03 14:11:29 +0300

    swipe

--- cosmeticshop/www/js/app.js
+++ cosmeticshop/www/js/app.js
@@ -7,9 +7,10 @@ var startApp = function() {
 	$("#hlPage1").click(function(){ window.location = "page1.html"; });
 	*/
 };
+var mySwiper = null;
 
 function initSwiper(){
-    var mySwiper = new Swiper('.swiper-container',{
+    mySwiper = new Swiper('.swiper-container',{
         pagination: '.pagination',
         paginationClickable: true,
         loop: true,
@@ -19,29 +20,31 @@ function initSwiper(){
     });
 }
 function initSwiperData(){
-    //$(document).bind('pagebeforeshow', function(e, data){
-    var svcurl = "http://feeds.delicious.com/v2/json/popular?callback=hello";      
-    $.ajax({url: svcurl,
-        dataType: "jsonp",
-        async: true,
-        success: function (result) {
-            ajax.parseJSONP(result);
-        },
-        error: function (request,error) {
-            alert('Network error has occurred please try again!');
-        }
-    });         
-    //});
-    
-    var ajax = {  
-        parseJSONP:function(result){
-            $.each(result, function(i, row) {
-                var tmp = $('#swipe-data').html();
-                tmp = tmp + '<div class="swiper-slide red-slide"><div class="title">' + row.d + '</div></div>';
-
-                $('#swipe-data').html(tmp);
-            });
-            initSwiper();
-        }
+	if (mySwiper == null) {
+	    //$(document).bind('pagebeforeshow', function(e, data){
+	    var svcurl = "http://feeds.delicious.com/v2/json/popular?callback=hello";      
+	    $.ajax({url: svcurl,
+	        dataType: "jsonp",
+	        async: true,
+	        success: function (result) {
+	            ajax.parseJSONP(result);
+	        },
+	        error: function (request,error) {
+	            alert('Network error has occurred please try again!');
+	        }
+	    });         
+	    //});
+	    
+	    var ajax = {  
+	        parseJSONP:function(result){
+	            $.each(result, function(i, row) {
+	                var tmp = $('#swipe-data').html();
+	                tmp = tmp + '<div class="swiper-slide red-slide"><div class="title">' + row.d + '</div></div>';
+	
+	                $('#swipe-data').html(tmp);
+	            });
+	            initSwiper();
+	        }
+	    };
     };
 }
