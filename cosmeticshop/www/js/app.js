125767707e71b30d076227637dec00045b7a4cc7
Mert Yerli <mertyerli@gmail.com>
2013-09-03 11:19:15 +0300

    swiper

--- cosmeticshop/www/js/app.js
+++ cosmeticshop/www/js/app.js
@@ -7,3 +7,14 @@ var startApp = function() {
 	$("#hlPage1").click(function(){ window.location = "page1.html"; });
 	*/
 };
+
+function initSwiper(){
+    var mySwiper = new Swiper('.swiper-container',{
+        pagination: '.pagination',
+        paginationClickable: true,
+        onTouchStart : function() {
+            //Do something when you touch the slide
+            //alert('OMG you touch the slide!');
+        }
+    });
+}
