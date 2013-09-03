02b481ce288becb58d2e05b2503fbf58f7f6f5cb
Mert Yerli <mertyerli@gmail.com>
2013-09-03 09:58:03 +0300

    ty

--- cosmeticshop/www/js/app.js
+++ cosmeticshop/www/js/app.js
@@ -1,8 +1,9 @@
 var startApp = function() {
-	//alert('started');
-	
+	alert('started');
+	/*
 	var el = document.getElementById("deviceready");
 	el.innerHTML = "device is ready";
 	   
 	$("#hlPage1").click(function(){ window.location = "page1.html"; });
+	*/
 };
