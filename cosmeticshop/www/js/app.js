cd1148ebb39e2b1eac31a8c0a19abcf2c011bdac
Mert Yerli <mertyerli@gmail.com>
2013-09-04 14:47:26 +0300

    maps

--- cosmeticshop/www/js/app.js
+++ cosmeticshop/www/js/app.js
@@ -61,4 +61,53 @@ function inAppBrowserTest(){
 	ref.addEventListener('exit', function(event) { alert(event.type); });
 }
 
-    
+
+function resizeMyContent() {
+	var hhtml = $('html').height();
+	var pageid = $.mobile.activePage.attr('id');
+	var hcontent = $('#' + pageid + ' div[data-role="content"]').height();
+	var posycontent = $('#' + pageid + ' div[data-role="content"]').offset().top;
+	var hcontentnew = hhtml - (posycontent * 2);
+	if (hcontent < hcontentnew) {
+		$('#' + pageid + ' div[data-role="content"]').css({
+			'height' : hcontentnew + 'px'
+		});
+	}
+}
+
+/*
+ * Google Maps documentation: http://code.google.com/apis/maps/documentation/javascript/basics.html
+ * Geolocation documentation: http://dev.w3.org/geo/api/spec-source.html
+ */
+function initMap(){
+//$( document ).on( "pageinit", "#map-page", function() {
+    var defaultLatLng = new google.maps.LatLng(34.0983425, -118.3267434);  // Default to Hollywood, CA when no geolocation support
+    if ( navigator.geolocation ) {
+        function success(pos) {
+            // Location found, show map with these coordinates
+            drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        }
+        function fail(error) {
+            drawMap(defaultLatLng);  // Failed to find location, show default map
+        }
+        // Find the users current position.  Cache the location for 5 minutes, timeout after 6 seconds
+        navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 500000, enableHighAccuracy:true, timeout: 6000});
+    } else {
+        drawMap(defaultLatLng);  // No geolocation support, show default map
+    }
+    function drawMap(latlng) {
+        var myOptions = {
+            zoom: 10,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+        // Add an overlay to the map of current lat/lng
+        var marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+            title: "Greetings!"
+        });
+    }
+//});
+}
