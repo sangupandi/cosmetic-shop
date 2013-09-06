ace72fb2bd790cb5ba463d069693201cf57f8955
Mert Yerli <mertyerli@gmail.com>
2013-09-06 14:09:07 +0300

    uhk

--- cosmeticshop/www/js/app.js
+++ cosmeticshop/www/js/app.js
@@ -158,6 +158,7 @@ var initMap = function() {
 	    	log("err:" + error.code);
             drawMap(defaultLatLng);  // Failed to find location, show default map
         }
+        log("Finding..");
         // Find the users current position.  Cache the location for 5 minutes, timeout after 6 seconds
         navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 3000, enableHighAccuracy:true, timeout: 6000});
     } else {
@@ -171,7 +172,7 @@ var initMap = function() {
             center: latlng,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-        log($("#map-canvas").height());
+        //log($("#map-canvas").height());
         var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
         // Add an overlay to the map of current lat/lng
         var marker = new google.maps.Marker({
