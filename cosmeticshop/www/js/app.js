a649d94f0034f5b28e3ea4081715d65ac006cbe4
Mert Yerli <mertyerli@gmail.com>
2013-09-09 16:12:26 +0300

    rqwer

--- cosmeticshop/www/js/app.js
+++ cosmeticshop/www/js/app.js
@@ -104,10 +104,8 @@ var getCurrentPosition = function() {
  * Geolocation documentation: http://dev.w3.org/geo/api/spec-source.html
  */
 var initMap = function() {
-    log("init map");
     
     var onGeoSuccess = function(position) {                
-        alert("success geo");
         var myLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
     
         map = new google.maps.Map(document.getElementById('map-canvas'), 
@@ -115,74 +113,36 @@ var initMap = function() {
                 mapTypeId: google.maps.MapTypeId.ROADMAP,
                 center: myLocation,
                 zoom: 15
-            }); 
+            });
+            
+        // Add an overlay to the map of current lat/lng
+        var marker = new google.maps.Marker({
+            position: myLocation,
+            map: map,
+            title: "Greetings!"
+        });
+
     };
+    
     var onGeoFail = function(error) {
         alert("err:" + error.code);
     };
     
     navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoFail);
     
-    /*
-    
 
-    var defaultLatLng = new google.maps.LatLng(34.0983425, -118.3267434);  // Default to Hollywood, CA when no geolocation support
-
-    if (navigator.geolocation) {
-        function success(pos) {
-            log("success map");
-            // Location found, show map with these coordinates
-            drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-        }
-        function fail(error) {
-            log("err:" + error.code);
-            drawMap(defaultLatLng);  // Failed to find location, show default map
-        }
-        log("navigator ok");
-        //log("Finding..");
-        // Find the users current position.  Cache the location for 5 minutes, timeout after 6 seconds
-        //navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 3000, enableHighAccuracy:true, timeout: 6000});
-        drawMap(defaultLatLng); 
-    } else {
-        log("No geolocation support");
-        drawMap(defaultLatLng);  // No geolocation support, show default map
-    }
-
-    function drawMap(latlng) {
-        var myOptions = {
-            zoom: 10,
-            center: latlng,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        //log($("#map-canvas").height());
-        var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-        log(map!=null?"not null":"null");
-        // Add an overlay to the map of current lat/lng
-        var marker = new google.maps.Marker({
-            position: latlng,
-            map: map,
-            title: "Greetings!"
-        });
-    }
-    */
 };
 
 var cameraTest = function() {
     var onCamSuccess = function (imageData) {
-        log("success cam");
-        //var image = document.getElementById('myImage');
-        //image.src = "data:image/jpeg;base64," + imageData;
+        /* No action required */
     };
 
     var onCamFail = function (error) {
-        log("err:" + error.code);
-        alert('Failed because: ' + error.code);
+        alert('Kamera kullanılamıyor (' + error.code + ')');
     };
 
-    log("Getting cam...");
-    console.log("Test Log");
-
-   // navigator.camera.cleanup(onCamSuccess, onCamFail);
+    //navigator.camera.cleanup(onCamSuccess, onCamFail);
     var cameraPopoverHandle = navigator.camera.getPicture(onCamSuccess, onCamFail, 
         {
             quality:25,
@@ -195,9 +155,6 @@ var cameraTest = function() {
             targetHeight: 80,
             saveToPhotoAlbum: false
         });
-        
-    //var cameraPopoverOptions = new CameraPopoverOptions(10, 10, 50, 50, 0);
-    //cameraPopoverHandle.setPosition(cameraPopoverOptions);
     
     $("#map-canvas").html("done cam . . .<br>");
 };
