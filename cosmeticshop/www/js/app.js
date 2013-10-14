b4289ffeb3b4f6530261c42fb8203ef5bb39486d
Mert Yerli <mertyerli@gmail.com>
2013-10-14 16:37:48 +0300

    ios7

--- cosmeticshop/www/js/app.js
+++ cosmeticshop/www/js/app.js
@@ -639,6 +639,10 @@ function startupSteps() {
 		});
 	});
 
+	// pass class for body, added element's class, 0 or 1
+	// 1 = for non ios7 testing
+	ios7StatusBarBump('ios7-detected','status-bar-bump',0);
+	
 	glog.step('startupSteps');
 }
 
@@ -662,3 +666,21 @@ function shareDebugLog() {
 function testOrientation() {
 	navigator.screenOrientation.set('landscape');
 }
+
+function ios7StatusBarBump(detectedClass,statusBarElementClass,nonIOS7test) {
+  var userAgentStr = /(iPad|iPhone);.*CPU.*OS 7_\d/i,
+      statusBarHtml = "<span class="+statusBarElementClass+"></span>";
+
+      if(navigator.userAgent.match(userAgentStr)){ // is ios7
+        addElement(detectedClass,statusBarHtml);
+      }
+  
+      if(nonIOS7test == 1){
+        addElement(detectedClass,statusBarHtml);
+      }
+
+      function addElement(detectedClass,statusBarHtml){
+        $('body').addClass(detectedClass).prepend(statusBarHtml);
+      }
+}
+
