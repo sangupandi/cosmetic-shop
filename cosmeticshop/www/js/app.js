a487598ee270f7e7619952f757c0962fdfede0c3
Mert Yerli <mertyerli@gmail.com>
2013-10-25 10:02:15 +0300

    4355

--- cosmeticshop/www/js/app.js
+++ cosmeticshop/www/js/app.js
@@ -23,7 +23,7 @@ function jsonLoader(_url, _successCallback, _errorCallback) {
 	this.url = _url;
 	this.successCallback = _successCallback;
 	this.errorCallback = _errorCallback;
-	
+
 	this.loaded = false;
 	this.trying = false;
 }
@@ -572,6 +572,55 @@ function openInAppBrowser(url) {
 	}
 }
 
+function isDate(txtDate) {
+	var currVal = txtDate;
+	if (currVal == '')
+		return false;
+
+	//Declare Regex
+	var rxDatePattern = /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/;
+	var dtArray = currVal.match(rxDatePattern);
+	// is format OK?
+
+	if (dtArray == null)
+		return false;
+
+	/*
+	//Checks for mm/dd/yyyy format.
+	dtMonth = dtArray[1];
+	dtDay = dtArray[3];
+	dtYear = dtArray[5];
+
+	//Checks for dd/mm/yyyy format.
+	dtDay = dtArray[1];
+	dtMonth= dtArray[3];
+	dtYear = dtArray[5];
+	*/
+
+	//Checks for yyyy/mm/dd format.
+	dtYear = dtArray[1];
+	dtMonth = dtArray[3];
+	dtDay = dtArray[5];
+
+	if (dtMonth < 1 || dtMonth > 12)
+		return false;
+	else if (dtDay < 1 || dtDay > 31)
+		return false;
+	else if ((dtMonth == 4 || dtMonth == 6 || dtMonth == 9 || dtMonth == 11) && dtDay == 31)
+		return false;
+	else if (dtMonth == 2) {
+		var isleap = (dtYear % 4 == 0 && (dtYear % 100 != 0 || dtYear % 400 == 0));
+		if (dtDay > 29 || (dtDay == 29 && !isleap))
+			return false;
+	}
+	return true;
+}
+
+function isValidEmailAddress(emailAddress) {
+	var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+	return pattern.test(emailAddress);
+};
+
 function postCustomerInfoForm() {
 	function showMessage(msg, title) {
 		navigator.notification.alert(msg, null, title, 'Tamam');
@@ -609,6 +658,10 @@ function postCustomerInfoForm() {
 		showMessage("Formu göndermek için ad soyad bilgilerinizi giriniz.", "Uyarı");
 		return;
 	}
+	if (dogumTarihi != "" && !isDate(dogumTarihi)) {
+		showMessage("Doğum tarihiniz için geçerli bir değer giriniz.", "Uyarı");
+		return;
+	}
 	if (smsAl && tel.length == 0) {
 		showMessage("Sms almak için telefon numaranızı giriniz.", "Uyarı");
 		return;
@@ -617,6 +670,11 @@ function postCustomerInfoForm() {
 		showMessage("Eposta almak için eposta adresinizi giriniz.", "Uyarı");
 		return;
 	}
+	if (epostaAl != "" && !isValidEmailAddress(epostaAl)) {
+		showMessage("Geçerli bir eposta adresi giriniz.", "Uyarı");
+		return;
+	}
+
 	if (tel.length == 0 && eposta.length == 0) {
 		showMessage("Formu göndermek için telefon numarası ya da eposta adresi bilgilerinden en az birini giriniz.", "Uyarı");
 		return;
@@ -626,8 +684,7 @@ function postCustomerInfoForm() {
 	var svcurl = serviceHost + "/CustomerForm.ashx?" + data;
 	var jl = new jsonLoader(svcurl, successFunc, errorFunc);
 	jl.load();
-	
-	
+
 	/*
 	 $.ajax({
 	 url : "ajax.php",
