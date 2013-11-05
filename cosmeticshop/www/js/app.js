714911277d89372b4583d6132759d1639a5edf94
Mert Yerli <mertyerli@gmail.com>
2013-11-05 11:07:19 +0200

    version update

--- cosmeticshop/www/js/app.js
+++ cosmeticshop/www/js/app.js
@@ -1,4 +1,4 @@
-var internalVersion = "Version 1.0.0 Build:838";
+var internalVersion = "Version 1.0.1 Build:839";
 //var serviceHost = "http://www.gtech.com.tr/cosmetica";
 var serviceHost = "http://www.cosmeticamobile.com";
 //var serviceHost = "http://37.230.108.2";
