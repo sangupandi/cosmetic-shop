0e1de7f5a1e9a28f5362fca43ee4ba869099449b
Mert Yerli <mertyerli@gmail.com>
2013-12-11 11:56:19 +0200

    homepage image change

--- cosmeticshop/www/js/app.js
+++ cosmeticshop/www/js/app.js
@@ -1,4 +1,4 @@
-var internalVersion = "Version 1.0.2 Build:871";
+var internalVersion = "Version 1.0.2 Build:872";
 
 //var serviceHost = "http://www.gtech.com.tr/cosmetica";
 var serviceHost = "http://www.cosmeticamobile.com";
