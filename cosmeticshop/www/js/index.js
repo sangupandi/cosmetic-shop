0c5a861f193a6632c39ca72a5541e346ed3e7011
Mert Yerli <mertyerli@gmail.com>
2014-02-12 21:26:19 +0200

    234

--- cosmeticshop/www/js/index.js
+++ cosmeticshop/www/js/index.js
@@ -93,7 +93,9 @@ var app = {
 	// The scope of 'this' is the event. In order to call the 'receivedEvent'
 	// function, we must explicity call 'app.receivedEvent(...);'
 	onDeviceReady : function() {
-		StatusBar.hide();
+		if (platform_iOS()) {
+			StatusBar.hide();
+		}		
 		app.receivedEvent('deviceready');
 	},
 
