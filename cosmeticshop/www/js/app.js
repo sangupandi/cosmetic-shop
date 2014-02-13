909ce04beeebf8cdae321aabea058f6e84194506
Mert Yerli <mertyerli@gmail.com>
2014-02-13 17:11:00 +0200

    2.9 fix

--- cosmeticshop/www/js/app.js
+++ cosmeticshop/www/js/app.js
@@ -1,4 +1,4 @@
-var internalVersion = "Version 1.0.3 Build:881";
+var internalVersion = "Version 1.0.5 Build:930";
 
 //var serviceHost = "http://www.gtech.com.tr/cosmetica";
 var serviceHost = "http://www.cosmeticamobile.com";
